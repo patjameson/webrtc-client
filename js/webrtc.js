561899781832f8b0ab09a89d858cefaad09efd51
Pat Jameson <patrickejameson@gmail.com>
2013-07-11 10:55:20 -0700

    WIP file transfer works for small files. N peer connection still flacky

--- js/webrtc.js
+++ js/webrtc.js
@@ -13,7 +13,9 @@ var _localStreamElement = null,
     _disconnectedMessages = [],
     _owner = true,
     _pcs = [],
-    _newConnectionCallback = null;
+    _dcs = [],
+    _newConnectionCallback = null,
+    cands = [];
 
 function WebRTC() {}
 
@@ -27,40 +29,38 @@ WebRTC.prototype = {
 
         if (type === 'both' || type === 'media' || typeof type === undefined) {
             thisWebRTC._startLocalStream(function () {
-                // pc.onaddstream = function (event) {
-                //  _remoteStreams[0] = event.stream;
-                //  attachMediaStream(_remoteStreamElements[0], _remoteStreams[0]);
-                //  _isStreaming = true;
-                // };
-
-                // if (type == 'both' || typeof type === undefined) {
-                //  dc1 = pc.createDataChannel('test', {'reliable': false});
-
-                //  thisWebRTC._startDataChannel();
-                // }
 
                 thisWebRTC._makeConnection(callback);
             });
         }
-        // } else if (type === 'data') {
-        //     dc1 = pc.createDataChannel('test', {'reliable': false});
-            
-        //     thisWebRTC._startDataChannel();
-
-        //     thisWebRTC._makeConnection(callback);
-        // }
     },
 
     onMessage: function (callback) {
-        //dc1.onmessage = callback;
+        var i;
+        for (i = 0;i < _dcs.length;i++) {
+            _dcs[i].onmessage = callback;
+        }
     },
 
     send: function (message) {
-        if (_isDataChannelOpen) {
-            dc1.send(message);
-        } else {
-            _disconnectedMessages.push(message);
+        var i;
+        for (i = 0;i < _dcs.length;i++) {
+            if (_dcs[i] !== undefined) {
+                _dcs[i].send(message);
+            } else {
+                _disconnectedMessages.push(message);
+            }
         }
+    },
+
+    sendFile: function (id) {
+        var thisWebRTC = this;
+        var reader = new FileReader();
+        reader.onload = function(e) {
+            var filename = document.getElementById(id).value.split(/(\/|\\)/).pop();
+            thisWebRTC.send(filename + "," + e.target.result);
+        }
+        reader.readAsText(document.getElementById(id).files[0]);
     },
 
     setLocal: function (element) {
@@ -122,14 +122,31 @@ WebRTC.prototype = {
         });
     },
 
-    _startDataChannel: function () {
-        dc1.onmessage = null;
+    _startDataChannel: function (id) {
+        _dcs[id] = _pcs[id].createDataChannel('test', {'reliable': false});
+        if (_dcs[0] !== undefined && _dcs[0].onmessage !== undefined) {
+            _dcs[id].onmessage = function(message) {
+                var splitComma = message.data.indexOf(",");
+                var filename = message.data.substring(0, splitComma);
+                var filedata = message.data.substring(splitComma+1);
+                uriContent = "data:application/octet-stream; filename=" + filename + "," + encodeURIComponent(filedata);
+                location.href = uriContent;
+            };
+        } else {
+            _dcs[id].onmessage = function(message) {
+                var splitComma = message.data.indexOf(",");
+                var filename = message.data.substring(0, splitComma);
+                var filedata = message.data.substring(splitComma+1);
+                uriContent = "data:application/octet-stream; filename=" + filename + "," + encodeURIComponent(filedata);
+                location.href = uriContent;
+            };
+        }
 
-        dc1.onopen = function () {
+        _dcs[id].onopen = function () {
             _isDataChannelOpen = true;
             var i;
             for (i = 0;i < _disconnectedMessages.length;i++) {
-                dc1.send(_disconnectedMessages[i]);
+                _dcs[id].send(_disconnectedMessages[i]);
             }
         };
     },
@@ -143,17 +160,14 @@ WebRTC.prototype = {
             client_id = data.client_id;
             id = data.id;
 
-            console.log('joined');
-
             var i;
             for (i = 0;i < client_id;i++) {
                 var configuration = {'iceServers': [DEFAULT_STUN_SERVER]};
                 var connection = {'optional': [{'RtpDataChannels': true }]};
                 _pcs[i] = new RTCPeerConnection(configuration, connection);
+                thisWebRTC._startDataChannel(i);
 
-                var curNumConn = i;
                 _pcs[i].onicecandidate = function (event) {
-                    console.log('onicecandidate');
                     if (event.candidate) {
                         socket.emit('cand', {'client_id': i, 'from_client_id': client_id, 'cand': event.candidate, 'id': id});
                     }
@@ -177,71 +191,68 @@ WebRTC.prototype = {
             client_id = data.from_client_id;
             id = data.id;
 
-            console.log('adddesc' + client_id);
+            console.log(data.client_id + " <------ " + client_id);
 
             var owner = false;
             if (_pcs[client_id] === undefined) {
                 var configuration = {'iceServers': [DEFAULT_STUN_SERVER]};
                 var connection = {'optional': [{'RtpDataChannels': true }]};
                 _pcs[client_id] = new RTCPeerConnection(configuration, connection);
+                thisWebRTC._startDataChannel(client_id);
                 owner = true;
             }
 
-            console.log('before');
             _pcs[client_id].addStream(_localStream);
 
-            console.log('before2');
             _pcs[client_id].onaddstream = function (event) {
                 _remoteStreams[_remoteStreams.length] = event.stream;
                 attachMediaStream(_remoteStreamElements[_remoteStreamElements.length-1], _remoteStreams[_remoteStreams.length-1]);
                 _isStreaming = true;
             };
 
-            console.log('before3');
-            var curNumConn = client_id;
             _pcs[client_id].onicecandidate = function (event) {
-                console.log('onicecandidatehere');
                 if (event.candidate) {
                     socket.emit('cand', {'client_id': client_id, 'from_client_id': data.client_id, 'cand': event.candidate, 'id': id});
                 }
             };
 
-            console.log('before4' + client_id);
-            console.log(data);
             if (_pcs[client_id] !== undefined)
                 _pcs[client_id].setRemoteDescription(new RTCSessionDescription(data.desc));
-            console.log('after');
 
             _streaming = true;
 
-            var curNumConn = client_id;
-            console.log(curNumConn);
-
-            console.log('before5');
             if (owner) {
-                _pcs[curNumConn].createAnswer(function (desc) {
-                    console.log('in here');
-                    _pcs[curNumConn].setLocalDescription(desc);
-                    console.log(data.client_id + " ---------------> " + data.from_client_id);
-                    socket.emit('desc', {'client_id': data.from_client_id, 'from_client_id': data.client_id, 'desc': desc, 'id': id, 'numConn': curNumConn});
+                console.log('owner');
+                _pcs[client_id].createAnswer(function (desc) {
+                    _pcs[client_id].setLocalDescription(desc);
+                    console.log(data.client_id + " ------> " + data.from_client_id);
+                    socket.emit('desc', {'client_id': data.from_client_id, 'from_client_id': data.client_id, 'desc': desc, 'id': id});
                 });
+            }
+
+            var i;
+            if (cands[client_id] !== undefined) console.log('numcand' + cands[client_id].length);
+            for (i = 0;cands[client_id] !== undefined && i < cands[client_id].length;i++) {
+                _pcs[client_id].addIceCandidate(new RTCIceCandidate(cands[i]));
             }
 
             _newConnectionCallback();
         });
 
         socket.on('add_cand', function (data) {
-            console.log('test');
-            console.log(new RTCIceCandidate(data.cand));
-            console.log(data.client_id);
-            _pcs[data.client_id].addIceCandidate(new RTCIceCandidate(data.cand));
+            if (_pcs[data.client_id] === undefined) {
+                if (cands[data.client_id] === undefined) cands[data.client_id] = [];
+                cands[data.client_id].push(data.cand);
+            } else {
+                _pcs[data.client_id].addIceCandidate(new RTCIceCandidate(data.cand));
+            }
         });
     },
 
     _updateDescription: function (pc, _client_id) {
         pc.createOffer(function (desc) {
             pc.setLocalDescription(desc);
-            console.log(client_id + " ---------------> " + _client_id);
+            console.log(client_id + " ------> " + _client_id);
             socket.emit('desc', {'client_id': _client_id, 'from_client_id': client_id, 'desc': desc, 'id': id});
         }, null);
     },
